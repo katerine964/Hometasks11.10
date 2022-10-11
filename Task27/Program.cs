53cd9b66163ff19a5ac8a393ee44b5868bfa1084
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-10-12 01:24:54 +0300

    Add files via upload

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+int m=0;
+while (n>0)
+{
+    m=m+n%10;
+    n=n/10;
+}
+Console.WriteLine("Сумма цифр данного числа равна "+m);
