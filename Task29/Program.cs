b4b47c91c1fa91f6ca3f3f911dd89fec2b272d88
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-10-12 01:19:17 +0300

    Add files via upload

---
+++ Task29/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+   Console.WriteLine("Введите элемент массива с номером "+ i);
+   array[i]=Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ",array)}]");
