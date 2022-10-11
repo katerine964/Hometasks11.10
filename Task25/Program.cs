b5e31888979dd6872c4d24f6af43dbe7b7511204
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-10-12 01:18:04 +0300

    Add files via upload

---
+++ Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Bведите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Bведите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c=1;
+for (int i = 0; i < b; i++)
+{
+    c=c*a;
+}
+Console.WriteLine("число "+a+" в степени "+b+" равно "+c);
